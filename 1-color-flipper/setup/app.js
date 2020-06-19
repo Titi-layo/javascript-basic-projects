4da275f33d6c07f848ccd3d01a94b79231d6226c
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MacBook-Pro.local>
2020-06-20 00:55:49 +0100

    Added in javascript to code

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+
+
+
+  document.getElementById("btn").onclick = function()
+  {
+    const index = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[index];
+    let colour = document.getElementsByClassName("color");
+    colour[0].innerHTML = colors[index];
+    colour[0].style.color = colors[index];
+  }
+  
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+document.getElementById("btn").onclick = function()
+  {
+    
+    let colour = `#`
+    for(let i = 0 ; i < 6 ; i++)
+    {
+      index = Math.floor(Math.random() * hex.length);
+      colour = colour + hex[index]
+    }
+    
+    document.body.style.backgroundColor = colour;
+    let temp = document.getElementsByClassName("color")
+    temp[0].innerHTML = colour;
+    
+  }
