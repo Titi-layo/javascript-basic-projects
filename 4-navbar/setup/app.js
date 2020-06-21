50c6420f591fe4ec430cc8ed3b6e151c334bd130
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MacBook-Pro.local>
2020-06-21 21:59:35 +0100

    even more efficient

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -10,10 +10,6 @@ const btn =  document.querySelector(".nav-toggle")
 const links = document.querySelector("ul")
 
 btn.addEventListener('click',function()
-{
-    if(links.classList.contains("show-links"))
-    links.classList.remove("show-links") 
-
-    else
-    links.classList.add("show-links")
+{ 
+    links.classList.toggle("show-links")
 })
