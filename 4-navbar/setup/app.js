84bae460362375a995b96bbd7d6f6d4858994913
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MacBook-Pro.local>
2020-06-21 21:47:52 +0100

    Added Javascript to Navbar project

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,17 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const btn2 =   document.getElementsByClassName("nav-toggle")
+const btn =  document.querySelector(".nav-toggle")
+const links = document.querySelector("ul")
+console.log(btn2)
+btn.addEventListener('click',function()
+{
+    if(links.className === "links")
+    links.className = "links show-links"
+
+    else
+    links.className = "links"
+})
